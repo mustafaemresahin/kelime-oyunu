47b0b29e4175b8b5438eaaabcb2ab2421c6e7283
Mustafa Emre Sahin <mustafa.sahin03@outlook.com>
2025-06-26 18:11:05 -0500

    Implement full game with timer and letter reveals

---
+++ questions.js
@@ -0,0 +1,12 @@
+const questions = [
+    { clue: "Y\u0131l\u0131n \u00fc\u00e7\u00fcnc\u00fc ay\u0131", answer: "mart" },
+    { clue: "Deniz kenar\u0131nda ince taneli yer", answer: "sahil" },
+    { clue: "Yasalar\u0131 yapan kurul", answer: "meclis" },
+    { clue: "Korkusuz, cesur ki\u015fi", answer: "yurekli" },
+    { clue: "\u0130nsanlar\u0131n ya\u015fad\u0131\u011f\u0131 yerle\u015fim yeri", answer: "yerlesim" },
+    { clue: "Sa\u011fl\u0131k alan\u0131nda uzmanl\u0131k", answer: "doktorluk" },
+    { clue: "Birden fazla hareket", answer: "hareketler" },
+    { clue: "Gazete yapma i\u015fi", answer: "gazetecilik" },
+    { clue: "M\u00fchendis olma mesle\u011fi", answer: "muhendislik" },
+    { clue: "Bilgisayar kodlama mesle\u011fi", answer: "programcilik" }
+];
